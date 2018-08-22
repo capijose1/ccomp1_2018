6e2efe33a2d691f96ffd291a59e64a6c9c5cc540
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:39:40 -0500

    quinta

--- juego/main.cpp
+++ juego/main.cpp
@@ -4,14 +4,25 @@ using namespace std;
 
 int main()
 {
+    /*
+    hola
+    */
     int a;
     int b;
     int c;
+    int d;
+    int e;
+    float f;
+
+
     cout << "inserte a ";
     cin >> a;
     cout << "inserte b ";
     cin >> b;
     c = a + b;
-    cout << "la suma de a + b es: "<< c << endl;
+    d = a * b;
+    e = a - b;
+    f = a / b;
+    cout << "la suma de a + b es: "<< c <<" la suma de a + b es: "<< c <<" la resta de a y b es: "<< e <<" la multiplicacion de a y b es: "<< d <<" la division de a y b es: "<< f << endl;
     return 0;
 }
