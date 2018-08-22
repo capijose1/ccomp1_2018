396939d104125568f90b5e9b5a9862769488a654
ALUMNO-E <ALUMNO-E@UCSP.AULAB>
2018-08-22 09:17:31 -0500

    quinta

--- juego/main.cpp
+++ juego/main.cpp
@@ -7,9 +7,11 @@ int main()
     int a;
     int b;
     int c;
+    cout << "inserte a ";
     cin >> a;
+    cout << "inserte b ";
     cin >> b;
     c = a + b;
-    cout << c << endl;
+    cout << "la suma de a + b es: "<< c << endl;
     return 0;
 }
